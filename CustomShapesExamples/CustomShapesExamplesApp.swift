fcacc88756ba51e1a041a80fbdfcc1958a9a8612
Madison Dellamea <mdellamea@lcs.on.ca>
2022-11-17 11:41:36 -0500

    Initial Commit

---
+++ CustomShapesExamples/CustomShapesExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesExamplesApp.swift
+//  CustomShapesExamples
+//
+//  Created by Madison Dellamea on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
