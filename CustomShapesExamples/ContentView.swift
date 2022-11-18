a63087652e01317e8c10662df6a3faf6185be242
Madison Dellamea <mdellamea@lcs.on.ca>
2022-11-18 14:18:06 -0500

    Complete diamond image, not recursive.

--- CustomShapesExamples/ContentView.swift
+++ CustomShapesExamples/ContentView.swift
@@ -7,15 +7,97 @@
 
 import SwiftUI
 
+struct Diagonal: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        // The path that describees shape!
+        var path = Path()
+        
+        // Send back (return) the path that describes the shape.
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        
+        return path
+        
+    }
+    
+}
+
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 300, y: 150))
+        path.addLine(to: CGPoint(x: 270, y: 180))
+        
+        path.move(to: CGPoint(x: 270, y: 180))
+        path.addLine(to: CGPoint(x: 350, y: 300))
+        
+        path.move(to: CGPoint(x: 350, y: 300))
+        path.addLine(to: CGPoint(x: 430, y: 180))
+        
+        path.move(to: CGPoint(x: 430, y: 180))
+        path.addLine(to: CGPoint(x: 400, y: 150))
+        
+        path.move(to: CGPoint(x: 400, y: 150))
+        path.addLine(to: CGPoint(x: 300, y: 150))
+        // diamond structure
+        path.move(to: CGPoint(x: 270, y: 180))
+        path.addLine(to: CGPoint(x: 300, y: 190))
+        
+        path.move(to: CGPoint(x: 300, y: 190))
+        path.addLine(to: CGPoint(x: 400, y: 190))
+        
+        path.move(to: CGPoint(x: 400, y: 190))
+        path.addLine(to: CGPoint(x: 430, y: 180))
+        // upper lines
+        path.move(to: CGPoint(x: 300, y: 190))
+        path.addLine(to: CGPoint(x: 320, y: 150))
+        
+        path.move(to: CGPoint(x: 320, y: 150))
+        path.addLine(to: CGPoint(x: 270, y: 180))
+        
+        path.move(to: CGPoint(x: 300, y: 190))
+        path.addLine(to: CGPoint(x: 350, y: 150))
+        
+        path.move(to: CGPoint(x: 350, y: 150))
+        path.addLine(to: CGPoint(x: 400, y: 190))
+        
+        path.move(to: CGPoint(x: 400, y: 190))
+        path.addLine(to: CGPoint(x: 380, y: 150))
+        
+        path.move(to: CGPoint(x: 380, y: 150))
+        path.addLine(to: CGPoint(x: 430, y: 180))
+        
+        path.move(to: CGPoint(x: 325, y: 190))
+        path.addLine(to: CGPoint(x: 350, y: 150))
+        
+        path.move(to: CGPoint(x: 375, y: 190))
+        path.addLine(to: CGPoint(x: 350, y: 150))
+        // lower lines
+        path.move(to: CGPoint(x: 300, y: 190))
+        path.addLine(to: CGPoint(x: 350, y: 300))
+        
+        path.move(to: CGPoint(x: 400, y: 190))
+        path.addLine(to: CGPoint(x: 350, y: 300))
+        
+        path.move(to: CGPoint(x: 325, y: 190))
+        path.addLine(to: CGPoint(x: 350, y: 300))
+        
+        path.move(to: CGPoint(x: 375, y: 190))
+        path.addLine(to: CGPoint(x: 350, y: 300))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Diamond()
+            .stroke()
+        
     }
 }
 
